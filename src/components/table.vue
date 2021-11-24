3a5895e0a16b3b2b019ed2601134c4810fd16967
shay-an <axy7779@outlook.com>
2021-11-24 23:47:07 +0800

    first commit

---
+++ src/components/table.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg">
+    <div class="table">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+  methods: {
+    show() {
+      this.isShow = false;
+    },
+  },
+};
+</script>
+
+<style>
+.bg {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+}
+.table {
+  position: fixed;
+  left: 0;
+  top: auto;
+  right: 0;
+  bottom: 0;
+  background: red;
+}
+</style>
